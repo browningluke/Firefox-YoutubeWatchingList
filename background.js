4cb9f5f231ffa57e846eef3a14da09e250d521bd
Luke Browning <browningluke01@gmail.com>
2024-07-29 00:25:33 -0700

    Add idb wrapper + get(all) fns

--- background.js
+++ background.js
@@ -17,6 +17,61 @@ const rootID = browser.contextMenus.create({
   contexts: ["all"],
 });
 
+// ---
+// IndexDB functions
+// ---
+
+function indexDBWrapper(fn) {
+  return new Promise(
+    function (resolve, reject) {
+      const request = indexedDB.open(DATABASE_NAME, DB_VERSION);
+
+      request.onupgradeneeded = function (event) {
+        reject(Error("DB not created!"));
+      };
+
+      request.onsuccess = function (event) {
+        const db = event.target.result;
+        // console.log("Database opened successfully");
+
+        const transaction = db.transaction([STORE_NAME], "readwrite");
+        const objectStore = transaction.objectStore(STORE_NAME);
+
+        const request = fn(objectStore);
+
+        request.onsuccess = function () {
+          // console.log("Transaction success!!");
+          resolve(request.result);
+        };
+
+        request.onerror = function (event) {
+          reject(Error(`Error adding object: ${event.target.error}`));
+        };
+      };
+
+      request.onerror = function (event) {
+        reject(Error(`Error opening database:  ${event.target.error}`));
+      };
+    }
+  );
+}
+
+function indexDBGetAll() {
+  return indexDBWrapper((objectStore) => {
+    return objectStore.getAll();
+  });
+}
+
+function indexDBGet(key) {
+  return indexDBWrapper((objectStore) => {
+    return objectStore.get(key);
+  });
+}
+
+// ---
+// Saving video
+// ---
+
 async function saveVideo(tabId) {
   let videoData = await browser.tabs.sendMessage(tabId, { action: "ywlGetVideoData" });
   // TODO Save video data
